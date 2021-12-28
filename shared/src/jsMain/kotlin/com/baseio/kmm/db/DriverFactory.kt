b32250992b4993d920a230f5ca8520eb6c7cbd3c
Anmol Verma <anmol.verma4@gmail.com>
2021-12-28 18:03:12 +0530

    TODO("Fix this! Koin can not init suspended method, find another way of providing sql driver")

--- shared/src/jsMain/kotlin/com/baseio/kmm/db/DriverFactory.kt
+++ shared/src/jsMain/kotlin/com/baseio/kmm/db/DriverFactory.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.await
 
 actual class DriverFactory {
     actual fun createDriver(): SqlDriver {
+        TODO("Fix this! Koin can not init suspended method, find another way of providing sql driver")
         return initSqlDriver(BaseIoDB.Schema).await()
     }
 }
--- shared/src/jsMain/kotlin/com/baseio/kmm/di/Koin.kt
+++ shared/src/jsMain/kotlin/com/baseio/kmm/di/Koin.kt
@@ -1,9 +1,13 @@
 package com.baseio.kmm.di
 
+import com.baseio.kmm.db.DriverFactory
 import org.koin.dsl.module
 import io.ktor.client.engine.js.*
 
 actual fun platformModule() = module {
     single { Js.create() }
+    single {
+        DriverFactory().createDriver()
+    }
 }
 

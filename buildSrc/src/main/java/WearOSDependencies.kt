e073dd46c4cec5271527e7d683a3357f2bc9b834
Shubham Singh <shubham.singh@mutualmobile.com>
2022-01-12 20:53:22 +0530

    Minor dependency version refactoring

--- buildSrc/src/main/java/WearOSDependencies.kt
+++ buildSrc/src/main/java/WearOSDependencies.kt
@@ -5,18 +5,23 @@ object WearOSDependencyVersions {
     const val legacySupport = "1.0.0"
     const val recyclerView = "1.2.1"
     const val wear = "1.2.0"
+    const val material = AndroidDependencyVersions.material
+    const val compose = AndroidDependencyVersions.compose
+    const val coil = AndroidDependencyVersions.coil
+    const val lifecycleRuntime = AndroidDependencyVersions.lifecycleRuntime
+    const val composeActivity = AndroidDependencyVersions.composeActivity
 }
 
 object WearOSDependencies {
     val implementation = listOf(
         "androidx.core:core-ktx:${WearOSDependencyVersions.core}",
         "com.google.android.gms:play-services-wearable:${WearOSDependencyVersions.playServicesWearable}",
-        "com.google.android.material:material:${AndroidDependencyVersions.material}",
-        "androidx.compose.ui:ui:${AndroidDependencyVersions.compose}",
-        "io.coil-kt:coil-compose:${AndroidDependencyVersions.coil}",
-        "androidx.compose.material:material:${AndroidDependencyVersions.compose}",
-        "androidx.lifecycle:lifecycle-runtime-ktx:${AndroidDependencyVersions.lifecycleRuntime}",
-        "androidx.activity:activity-compose:${AndroidDependencyVersions.composeActivity}",
+        "com.google.android.material:material:${WearOSDependencyVersions.material}",
+        "androidx.compose.ui:ui:${WearOSDependencyVersions.compose}",
+        "io.coil-kt:coil-compose:${WearOSDependencyVersions.coil}",
+        "androidx.compose.material:material:${WearOSDependencyVersions.compose}",
+        "androidx.lifecycle:lifecycle-runtime-ktx:${WearOSDependencyVersions.lifecycleRuntime}",
+        "androidx.activity:activity-compose:${WearOSDependencyVersions.composeActivity}",
         "androidx.percentlayout:percentlayout:${WearOSDependencyVersions.percentLayout}",
         "androidx.legacy:legacy-support-v4:${WearOSDependencyVersions.legacySupport}",
         "androidx.recyclerview:recyclerview:${WearOSDependencyVersions.recyclerView}",

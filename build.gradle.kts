59fbbb4490c974a3fc028147cd84ac8653980d68
Shubham Singh <shubham.singh@mutualmobile.com>
2022-01-12 20:30:50 +0530

    Migrated WearOS dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 buildscript {
-    val compose_version by extra("1.1.0-rc02")
     repositories {
         gradlePluginPortal()
         google()
--- buildSrc/src/main/java/WearOSDependencies.kt
+++ buildSrc/src/main/java/WearOSDependencies.kt
@@ -1,2 +1,34 @@
-object WearOSDependencyVersions
-object WearOSDependencies
+object WearOSDependencyVersions {
+    const val core = "1.7.0"
+    const val playServicesWearable = "17.1.0"
+    const val percentLayout = "1.0.0"
+    const val legacySupport = "1.0.0"
+    const val recyclerView = "1.2.1"
+    const val wear = "1.2.0"
+}
+
+object WearOSDependencies {
+    val implementation = listOf(
+        "androidx.core:core-ktx:${WearOSDependencyVersions.core}",
+        "com.google.android.gms:play-services-wearable:${WearOSDependencyVersions.playServicesWearable}",
+        "com.google.android.material:material:${AndroidDependencyVersions.material}",
+        "androidx.compose.ui:ui:${AndroidDependencyVersions.compose}",
+        "io.coil-kt:coil-compose:${AndroidDependencyVersions.coil}",
+        "androidx.compose.material:material:${AndroidDependencyVersions.compose}",
+        "androidx.lifecycle:lifecycle-runtime-ktx:${AndroidDependencyVersions.lifecycleRuntime}",
+        "androidx.activity:activity-compose:${AndroidDependencyVersions.composeActivity}",
+        "androidx.percentlayout:percentlayout:${WearOSDependencyVersions.percentLayout}",
+        "androidx.legacy:legacy-support-v4:${WearOSDependencyVersions.legacySupport}",
+        "androidx.recyclerview:recyclerview:${WearOSDependencyVersions.recyclerView}",
+        "androidx.wear:wear:${WearOSDependencyVersions.wear}",
+    )
+}
+object WearOSPlugins {
+    val plugins = listOf(
+        "com.android.application",
+        "kotlin-android",
+    )
+    val kotlinPlugins = listOf(
+        "android"
+    )
+}

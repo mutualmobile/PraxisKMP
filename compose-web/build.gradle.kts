e02a12e440222a2e561cf571e2e0020d2b87100b
Shubham Singh <shubham.singh@mutualmobile.com>
2022-05-05 11:11:32 +0530

    Implemented compose-web UI

--- compose-web/build.gradle.kts
+++ compose-web/build.gradle.kts
@@ -22,6 +22,7 @@ kotlin {
             dependencies {
                 implementation(compose.web.core)
                 implementation(compose.runtime)
+                implementation(project(":shared"))
             }
         }
     }
